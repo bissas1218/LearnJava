23157417f02c6c5974a9b38f204b29008a3aeb0e
kbs <kbs@DESKTOP-9FI8EMQ>
2022-04-25 11:23:54 +0900

    basic

--- src/firstJavaApp/singleFilePrrm/HelloJava.java
+++ src/firstJavaApp/singleFilePrrm/HelloJava.java
@@ -15,4 +15,14 @@ public class HelloJava {
 		System.out.println("Hello " + args[0]);
 	}
 
+	/*
+	 * Shebang 파일로 실행
+     * Unix와 같은 운영 체제에서 단일 파일 소스 코드 응용 프로그램은 스크립트와 같은 shebang 파일로 시작할 수도 있습니다. 
+     * 자바 소스 파일 내에서 파일의 첫 번째 줄로 path/to/java/home --source <version>아래 예제와 같이 추가합니다.
+     * 
+     * 파일은 .java파일 확장자로 사용할 수 없으며 실행 가능해야 합니다 chmod +x. 이를 통해 다음과 같이 시작할 수 있습니다.
+     * 
+     * ./HelloJava
+     * 
+	 */
 }
--- src/firstJavaApp/singleFilePrrm/ReferenceNonJDKClass.java
+++ src/firstJavaApp/singleFilePrrm/ReferenceNonJDKClass.java
@@ -2,6 +2,12 @@ package firstJavaApp.singleFilePrrm;
 
 import org.apache.commons.lang3.RandomUtils;
 
+/*
+ * 그러나 Apache Commons LangRandomUtils 의 일부 를 참조하는 아래 예제 는 시작 시 클래스 경로에 추가해야 합니다 .commons-lang.jar
+ * 
+ * java -cp /path/to/commons-lang3-3.12.0.jar ReferenceNonJDKClass.java
+ * 
+ */
 public class ReferenceNonJDKClass {
 
 	public static void main(String[] args) {
